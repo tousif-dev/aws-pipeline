32ed8bcc2911d3a848a67fb3356c908a350e69ec
prathamesh199728 <79826384+prathamesh199728@users.noreply.github.com>
2022-05-20 12:57:39 +0530

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 676847358676.dkr.ecr.us-east-1.amazonaws.com
 docker pull 676847358676.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
