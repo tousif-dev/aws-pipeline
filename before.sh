17fb1503ea44aaa16f042baa3c68562d31f02047
Mohammad Tousif <69017259+tousif-dev@users.noreply.github.com>
2022-06-13 18:58:25 +0530

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 676847358676.dkr.ecr.us-west-2.amazonaws.com
-docker pull 676847358676.dkr.ecr.us-west-2.amazonaws.com/helloworld:latest
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 839710380783.dkr.ecr.us-west-2.amazonaws.com
+docker pull 839710380783.dkr.ecr.us-west-2.amazonaws.com/helloworld:latest
