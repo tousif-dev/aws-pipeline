7c521fab2764c9ac671144d09bcb0ebe0ba8a94a
Mohammad Tousif <69017259+tousif-dev@users.noreply.github.com>
2022-06-13 18:56:46 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm nginx -f
-docker run --name nginx  -d -p 80:80 -it 676847358676.dkr.ecr.us-east-1.amazonaws.com/helloworld
+docker run --name nginx  -d -p 80:80 -it 676847358676.dkr.ecr.us-west-2.amazonaws.com/helloworld
 echo "Pipeline constructed well"
