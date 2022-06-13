031843c587324f9ee1603660c5fe63de5ed2342b
Mohammad Tousif <69017259+tousif-dev@users.noreply.github.com>
2022-06-13 18:34:17 +0530

    Update before.sh
    edited before script to us-west-2

--- before.sh
+++ before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 676847358676.dkr.ecr.us-east-1.amazonaws.com
-docker pull 676847358676.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 676847358676.dkr.ecr.us-east-1.amazonaws.com
+docker pull 676847358676.dkr.ecr.us-west-1.amazonaws.com/helloworld:latest
